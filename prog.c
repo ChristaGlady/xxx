b77f2d28817e48108547f640c6f95abd8e6d1afb
ChristaGlady <christajglady7@gmail.com>
2016-06-25 07:06:00 +0530

    Update prog.c

--- prog.c
+++ prog.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include <conio.h>
+ 
+int main(){
+int number;
+clrscr();
+printf("Enter a number \n");
+scanf("%d", &number);
+if(number%2 == 1){
+printf("%d is Odd Number\n", number);
+}
+else {
+printf("%d is Even Number\n", number);
+ }
+getch();
+return 0;
